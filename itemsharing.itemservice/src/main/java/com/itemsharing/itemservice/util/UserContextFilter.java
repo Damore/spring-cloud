ed2178cae0bd8e2bc2af4497b44e0fa98fe9bf2b
gabriel.marciano <gabriel.marciano@CONTW-DES54.CONTMATIC.AD>
2017-12-28 17:47:24 -0200

    Capítulo 6

---
+++ itemsharing.itemservice/src/main/java/com/itemsharing/itemservice/util/UserContextFilter.java
@@ -0,0 +1,49 @@
+package com.itemsharing.itemservice.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserContextFilter implements Filter {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
+	
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest serveletRequest, ServletResponse serveletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest httpServletRequest = (HttpServletRequest) serveletRequest;
+		
+		UserContextHolder.getContext().setCorrelationId(httpServletRequest.getHeader(UserContext.CORRELATION_ID));
+		UserContextHolder.getContext().setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN));
+		UserContextHolder.getContext().setUserId(httpServletRequest.getHeader(UserContext.USER_ID));
+		
+		logger.debug("UserContextFilter Correlationid: {}", UserContextHolder.getContext().getCorrelationId());
+		
+		filterChain.doFilter(httpServletRequest, serveletResponse);
+		
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
